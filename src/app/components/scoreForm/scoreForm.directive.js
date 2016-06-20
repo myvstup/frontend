3d305dc712bde59b801ccb15e0c85422db9cb538
Vasyl Posmit <vasyl.posmit@gmail.com>
2016-06-20 19:45:51 +0300

    clean up the mess after forced merge

--- src/app/components/scoreForm/scoreForm.directive.js
+++ src/app/components/scoreForm/scoreForm.directive.js
@@ -46,25 +46,10 @@ export function scoreFormDirective() {
 }
 
 class scoreFormController {
-  constructor ($log, githubContributor) {
+  constructor (userService) {
     'ngInject';
     let vm = this;
 
-    this.$log = $log;
-
-    vm.userData = {
-      ukr: { caption: 'Українська мова і література', isTested: true},
-      history: { caption: 'Історія України', isTested: false},
-      math: { caption: 'Математика', isTested: false},
-      biology: { caption: 'Біологія', isTested: false},
-      geography: { caption: 'Географія', isTested: false},
-      physics: { caption: 'Фізика', isTested: false},
-      chemistry: { caption: 'Хімія', isTested: false},
-      eng: { caption: 'Англійська мова', isTested: false},
-      es: { caption: 'Іспанська мова', isTested: false},
-      de: { caption: 'Німецька мова', isTested: false},
-      ru: { caption: 'Російська мова', isTested: false},
-      fr: { caption: 'Французька мова', isTested: false}
-    };
+    vm.userData = userService.userData;
   }
 }
