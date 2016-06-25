9ca4e1ca46f475fa5c0f7d156cf2eac03afa32aa
Vasyl Posmit <vasyl.posmit@gmail.com>
2016-06-25 13:17:31 +0300

    refactor: cleaning after Yeoman

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,26 +4,16 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './pages/main.controller';
-// import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-// import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { apiService } from '../app/common/apiService';
 import { userService } from '../app/common/userService';
-// import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-// import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 import { scoreFormDirective } from '../app/components/scoreForm/scoreForm.directive';
 
 
 angular.module('myVstup', ['ngAria', 'restangular', 'ui.router', 'ngMaterial', 'toastr'])
-  // .constant('malarkey', malarkey)
-  .constant('moment', moment)
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  // .service('githubContributor', GithubContributorService)
-  // .service('webDevTec', WebDevTecService)
   .service('apiService', apiService)
   .service('userService', userService)
   .controller('MainController', MainController)
-  // .directive('acmeNavbar', NavbarDirective)
-  // .directive('acmeMalarkey', MalarkeyDirective)
   .directive('scoreForm', scoreFormDirective);
--- src/app/pages/main.controller.js
+++ src/app/pages/main.controller.js
@@ -8,50 +8,22 @@ export class MainController {
     controller.api = apiService;
     controller.userData = userService.userData;
 
-    controller.awesomeThings = [];
-    controller.classAnimation = '';
-    controller.creationDate = 1465734467013;
-    // controller.toastr = toastr;
-    console.log("constructor this"); console.log(this);
-    // this.activate($timeout, webDevTec);
 }
 
   postScores(data, config) {
     const controller = this;
     return this.api.postSome('points', data, config)
             .then(function(response) {
-              console.log("POST data response"); console.log(response.data);
               controller.userData.id = response.data.userId;
             });
   }
 
   getUserId() {
     const controller = this;
-    console.log("controller.userData"); console.log(controller.userData);
     return this.api.getSome('points')
             .then(function(response) {
-              console.log("GET data response"); console.log(response.data);
               controller.userData.id = response.data.userId;
             });
   }
 
-  // activate($timeout, webDevTec) {
-  //   this.getWebDevTec(webDevTec);
-  //   $timeout(() => {
-  //     this.classAnimation = 'rubberBand';
-  //   }, 4000);
-  // }
-  //
-  // getWebDevTec(webDevTec) {
-  //   this.awesomeThings = webDevTec.getTec();
-  //
-  //   angular.forEach(this.awesomeThings, (awesomeThing) => {
-  //     awesomeThing.rank = Math.random();
-  //   });
-  // }
-  //
-  // showToastr() {
-  //   this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-  //   this.classAnimation = '';
-  // }
 }
